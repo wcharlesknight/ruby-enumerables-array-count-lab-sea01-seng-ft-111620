c62dabde78c75443f4f24c10474e5d8dd18d2bdb
Charlie Knight <williamchknight@gmail.com>
2020-11-07 16:55:32 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -5,8 +5,8 @@ def count_strings(array)
 end
 
 def count_empty_strings(array)
-  empty_string = " "
   array.count do |element|
-    element.is_a? empty_string
+    if element == " "
+  end
   end 
 end
